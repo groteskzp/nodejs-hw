13d65f64b0842dda893ca556e1da2750cc222b9f
grotesk <groteskzp@gmail.com>
2026-08-21 14:16:32 +0300

    fix: update return option in updateNote function for consistency

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -37,7 +37,9 @@ export const deleteNote = async (req, res) => {
 
 export const updateNote = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findByIdAndUpdate(noteId, req.body, { new: true });
+  const note = await Note.findByIdAndUpdate(noteId, req.body, {
+    returnDocument: 'after',
+  });
 
   if (!note) {
     throw createHttpError(404, 'Note not found');

a24ed48f76826dd848d89f7e40d44e1081362541
grotesk <groteskzp@gmail.com>
2026-09-05 23:53:07 +0300

    fix: address auth homework feedback
    Use a compound tag+userId index, authenticate with both sessionId and accessToken, and clear expired refresh sessions before returning 401.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -58,6 +58,10 @@ export const refreshUserSession = async (req, res) => {
   }
 
   if (new Date() > session.refreshTokenValidUntil) {
+    await Session.deleteOne({ _id: session._id });
+    res.clearCookie('sessionId');
+    res.clearCookie('accessToken');
+    res.clearCookie('refreshToken');
     throw createHttpError(401, 'Session token expired');
   }
 

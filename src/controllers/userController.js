068ae8b10a4945eb53d3d47872e6de6214507665
grotesk <groteskzp@gmail.com>
2026-09-13 12:21:52 +0300

    fix: address mail and avatar homework feedback
    Require min password length on reset, set SMTP_FROM explicitly when sending reset email, and return the updated user after avatar upload.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -9,9 +9,15 @@ export const updateUserAvatar = async (req, res) => {
 
   const result = await saveFileToCloudinary(req.file.buffer, req.user._id);
 
-  await User.findByIdAndUpdate(req.user._id, {
-    avatar: result.secure_url,
-  });
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      avatar: result.secure_url,
+    },
+    {
+      returnDocument: 'after',
+    },
+  );
 
   res.status(200).json({
     url: result.secure_url,
